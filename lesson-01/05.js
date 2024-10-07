887e03cf20c8fb4b453c829164b74930a74d6b2c
Evgeny <153275378+cosackHector@users.noreply.github.com>
2024-10-07 21:13:16 +0300

    add breack in switch

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -3,8 +3,11 @@ let lng = rule.length
 switch(lng) {
     case lng < 25:
         console.log("Все таки нет правил без исключения");
+        break;
     case lng > 25:
         console.log("У меня 100 пудов все получиться");
+        break;
     case lng = 25:
         console.log("50 на 50");
+        break;
 };
