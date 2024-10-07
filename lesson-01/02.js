c8367a28526a7b7af453f246dd43212032c8e240
Evgeny <153275378+cosackHector@users.noreply.github.com>
2024-10-07 19:15:00 +0300

    ref

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,5 +1,4 @@
 // code
 const number = Math.floor(Math.random() * 100);
-const str = number % 2 === 0 ? 'Это четное число' : 'Это нечетное число'
 
-console.log(number, str);
+console.log(`${number}, ${number % 2 === 0 ? 'Это четное число' : 'Это нечетное число'}`);
