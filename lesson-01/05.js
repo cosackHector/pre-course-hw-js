2e1238ae5d687931b0edefe49b97c8f387258b1e
Evgeny <153275378+cosackHector@users.noreply.github.com>
2024-10-07 21:20:07 +0300

    ref

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,13 +1,13 @@
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
-let lng = rule.length
-switch(lng) {
-    case lng < 25:
-        console.log("Все таки нет правил без исключения");
+switch(rule.length) {
+    case rule.length < 25:
+        console.log("Все таки нет правил без исключения" );
         break;
-    case lng > 25:
-        console.log("У меня 100 пудов все получиться");
+    case rule.length > 25:
+        console.log("У меня 100 пудов все получится");
         break;
-    case lng = 25:
+    case rule.length = 25:
         console.log("50 на 50");
         break;
+        default: rule;
 };
