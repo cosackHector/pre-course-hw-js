4a4386617cb5382faa7497b5f6165e483ea661d6
Evgeny <153275378+cosackHector@users.noreply.github.com>
2024-10-09 02:05:49 +0300

    exc 6

---
+++ lesson-02/06.js
@@ -0,0 +1,14 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let newPassport = structuredClone(passportWithAddress);
+newPassport.address.city = 'Bobryisk'
+
+console.log(passportWithAddress.address.city);
+console.log(newPassport.address.city);
